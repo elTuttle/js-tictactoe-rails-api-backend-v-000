7cc182497196987ff9dfcbf304edf0679e61fd3f
John Tuttle <thejwt@gmail.com>
2017-12-18 19:29:03 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -13,6 +13,10 @@ class GamesController < ApplicationController
 
   def show
     @game = Game.find(params[:id])
+    respond_to do |format|
+      format.html { render :show }
+      format.json { render json: @game }
+    end
   end
 
   def update
