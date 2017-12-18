b14e90ff1b236a8a8d7c2446203fb0154d338b75
John Tuttle <thejwt@gmail.com>
2017-12-18 19:16:26 +0000

    automatically backed up by learn

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -2,7 +2,7 @@ class GamesController < ApplicationController
   # Add your GamesController code here
 
   def create
-    binding.pry
+    #binding.pry
     @game = Game.create(state: params[:state])
     game_path @game
   end
